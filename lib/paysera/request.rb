e9d45bf9f5a8bd20b8f0f0bcd3eb5a474c7617d9
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2014-12-29 00:38:50 +0200

    made query generation

--- lib/paysera/request.rb
+++ lib/paysera/request.rb
@@ -1,33 +1,62 @@
+require 'base64'
+require 'digest/md5'
+
 class Paysera::Request
   def self.build_request(paysera_params)
     # Ensure that all key will be symbols
-    attributes                 = Hash[paysera_params.map { |k, v| [k.to_sym, v] }]
+    request_params              = Hash[paysera_params.map { |k, v| [k.to_sym, v] }]
 
-    # Set default attributes
-    attributes[:version]       = Paysera::API_VERSION
-    attributes[:project_id]    ||= 'TODO'
-    attributes[:sign_password] ||= 'TODO'
+    # Set default values 
+    request_params[:version]    = Paysera::API_VERSION
+    request_params[:project_id] = 123123123 # TODO get project_id from rails configuration file
 
-    # Validate attributes
-    validate_data(attributes)
+    encoded_query  = encode_query(validate_and_make_query(request_params))
+    signed_request = sign_request(encoded_query, '34nig3uov30') # TODO get sign password from rails configuration file
+
+    query = "data=#{encoded_query}&sign=#{signed_request}"
+
+    query
   end
 
   private
 
-  def self.validate_data(attributes)
-    Paysera::Attributes::REQUEST.each do |k, v|
-      raise e("'#{k}' is required but missing") if v[:required] and attributes[k].nil?
-
-      unless attributes[k].nil?
-        # raise "#{attributes[k]} lasd"
-        raise e("'#{k}' value '#{attributes[k]}' is too long, #{v[:maxlen]} characters allowed.") if v[:maxlen] and attributes[k].length > v[:maxlen]
-        # raise e("'#{k}' value '#{attributes[k]}' invalid.") if '' != v[:regex] and attributes[k].to_s.match(v[:regex])
-        raise e("'#{k}' value '#{attributes[k]}' invalid.") unless v[:regex].nil? and attributes[k].to_s.match(v[:regex])
-      end
-    end
+  def self.sign_request(query, password)
+    # Encode string + password with md5
+    Digest::MD5.hexdigest(query + password)
   end
 
-  def self.e(msg)
+  def self.encode_query(query)
+    # 1) Encode with base64
+    # 2) Replace / with _ and + with -
+    Base64.encode64(query).gsub('/', '_').gsub('+', '-')
+  end
+
+  def self.validate_and_make_query(req)
+    request = ''
+
+    Paysera::Attributes::REQUEST.each do |k, v|
+      raise exception("'#{k}' is required but missing") if v[:required] and req[k].nil?
+
+      req_value = req[k].to_s
+      regex = v[:regex].to_s
+      maxlen = v[:maxlen]
+
+      unless req[k].nil?
+        raise exception("'#{k}' value '#{req[k]}' is too long, #{v[:maxlen]} characters allowed.") if maxlen and req_value.length > maxlen
+        raise exception("'#{k}' value '#{req[k]}' invalid.") if '' != regex and !req_value.match(regex)
+
+        # Make query
+        request += "&#{k}=#{req[k]}"
+      end
+    end
+
+    # Delete first char - &
+    request[0] = ''
+
+    request
+  end
+
+  def self.exception(msg)
     Exception.new msg
   end
 end
