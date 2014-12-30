3cc5e4c9438371e65551048bce59a87845ca4835
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2014-12-30 20:37:12 +0200

    moved functions from helper & some minor changes

--- lib/paysera/request.rb
+++ lib/paysera/request.rb
@@ -1,26 +1,25 @@
 require 'base64'
+require 'cgi'
 require 'digest/md5'
 
 class Paysera::Request
-  def self.build_request(paysera_params, sign_password='')
+  def self.build_request(paysera_params, sign_password=nil)
     # Ensure that all key will be symbols
     request_params = Hash[paysera_params.map { |k, v| [k.to_sym, v] }]
-
-    helper                     = Paysera::Helper
 
     # Set default values 
     request_params[:version]   = Paysera::API_VERSION
     request_params[:projectid] ||= Paysera.project_id
-    sign_password              = Paysera.sign_password
+    sign_password  ||= Paysera.sign_password
 
     valid_request  = validate_request(request_params)
-    encoded_query  = encode_query helper.make_query(valid_request)
+    encoded_query  = encode_string make_query(valid_request)
     signed_request = sign_request(encoded_query, sign_password)
 
-    query = helper.make_query({
-                                  :data => encoded_query,
-                                  :sign => signed_request
-                              })
+    query = make_query({
+                           :data => encoded_query,
+                           :sign => signed_request
+                       })
 
 
     "https://www.mokejimai.lt/pay/?#{query}"
@@ -28,15 +27,21 @@ class Paysera::Request
 
   private
 
+  def self.make_query(data)
+    data.collect do |key, value|
+      "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
+    end.compact.sort! * '&'
+  end
+
   def self.sign_request(query, password)
     # Encode string + password with md5
     Digest::MD5.hexdigest(query + password)
   end
 
-  def self.encode_query(query)
+  def self.encode_string(string)
     # 1) Encode with base64
     # 2) Replace / with _ and + with -
-    Base64.encode64(query).gsub("\n", '').gsub('/', '_').gsub('+', '-')
+    Base64.encode64(string).gsub("\n", '').gsub('/', '_').gsub('+', '-')
   end
 
   def self.validate_request(req)
