6e9d893cce8b69799f676ebcf1a5b3d64abd33c9
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2014-12-30 20:36:20 +0200

    moved function elsewhere

--- lib/paysera/helper.rb
+++
@@ -1,9 +0,0 @@
-require 'cgi'
-
-class Paysera::Helper
-  def self.make_query(data)
-    data.collect do |key, value|
-      "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
-    end.compact.sort! * '&'
-  end
-end
