bc37db51123de81d1ec2e857cf95b1d89b93d9a4
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2015-01-05 21:45:31 +0200

    exception() replaced with send_error(), namespace removed from class name and class added to module

--- lib/paysera/request.rb
+++ lib/paysera/request.rb
@@ -2,71 +2,73 @@ require 'base64'
 require 'cgi'
 require 'digest/md5'
 
-class Paysera::Request
-  def self.build_request(paysera_params, sign_password=nil)
-    # Ensure that all key will be symbols
-    request_params = Hash[paysera_params.map { |k, v| [k.to_sym, v] }]
+module Paysera
+  class Request
+    def build_request(paysera_params, sign_password=nil)
+      # Ensure that all key will be symbols
+      request_params             = Hash[paysera_params.map { |k, v| [k.to_sym, v] }]
 
-    # Set default values 
-    request_params[:version]   = Paysera::API_VERSION
-    request_params[:projectid] ||= Paysera.project_id
-    sign_password  ||= Paysera.sign_password
+      # Set default values
+      request_params[:version]   = Paysera::API_VERSION
+      request_params[:projectid] ||= Paysera.project_id
+      sign_password              ||= Paysera.sign_password
 
-    valid_request  = validate_request(request_params)
-    encoded_query  = encode_string make_query(valid_request)
-    signed_request = sign_request(encoded_query, sign_password)
+      valid_request  = validate_request(request_params)
+      encoded_query  = encode_string make_query(valid_request)
+      signed_request = sign_request(encoded_query, sign_password)
 
-    query = make_query({
-                           :data => encoded_query,
-                           :sign => signed_request
-                       })
+      query = make_query({
+                             :data => encoded_query,
+                             :sign => signed_request
+                         })
 
 
-    "https://www.mokejimai.lt/pay/?#{query}"
-  end
-
-  private
-
-  def self.make_query(data)
-    data.collect do |key, value|
-      "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
-    end.compact.sort! * '&'
-  end
-
-  def self.sign_request(query, password)
-    # Encode string + password with md5
-    Digest::MD5.hexdigest(query + password)
-  end
-
-  def self.encode_string(string)
-    # 1) Encode with base64
-    # 2) Replace / with _ and + with -
-    Base64.encode64(string).gsub("\n", '').gsub('/', '_').gsub('+', '-')
-  end
-
-  def self.validate_request(req)
-    request = {}
-
-    Paysera::Attributes::REQUEST.each do |k, v|
-      raise exception("'#{k}' is required but missing") if v[:required] and req[k].nil?
-
-      req_value = req[k].to_s
-      regex     = v[:regex].to_s
-      maxlen    = v[:maxlen]
-
-      unless req[k].nil?
-        raise exception("'#{k}' value '#{req[k]}' is too long, #{v[:maxlen]} characters allowed.") if maxlen and req_value.length > maxlen
-        raise exception("'#{k}' value '#{req[k]}' invalid.") if '' != regex and !req_value.match(regex)
-
-        # Add only existing params
-        request[k] = req[k]
-      end
+      "https://www.mokejimai.lt/pay/?#{query}"
     end
 
-    request
-  end
+    private
 
-  def self.exception(msg)
-    Exception.new msg
+    def make_query(data)
+      data.collect do |key, value|
+        "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
+      end.compact.sort! * '&'
+    end
+
+    def sign_request(query, password)
+      # Encode string + password with md5
+      Digest::MD5.hexdigest(query + password)
+    end
+
+    def encode_string(string)
+      # 1) Encode with base64
+      # 2) Replace / with _ and + with -
+      Base64.encode64(string).gsub("\n", '').gsub('/', '_').gsub('+', '-')
+    end
+
+    def validate_request(req)
+      request = {}
+
+      Paysera::Attributes::REQUEST.each do |k, v|
+        raise send_error("'#{k}' is required but missing") if v[:required] and req[k].nil?
+
+        req_value = req[k].to_s
+        regex     = v[:regex].to_s
+        maxlen    = v[:maxlen]
+
+        unless req[k].nil?
+          raise send_error("'#{k}' value '#{req[k]}' is too long, #{v[:maxlen]} characters allowed.") if maxlen and req_value.length > maxlen
+          raise send_error("'#{k}' value '#{req[k]}' invalid.") if '' != regex and !req_value.match(regex)
+
+          # Add only existing params
+          request[k] = req[k]
+        end
+      end
+
+      request
+    end
+
+    def send_error(msg)
+      Paysera::Error::Request.new msg
+    end
   end
 end
--- lib/paysera/response.rb
+++ lib/paysera/response.rb
@@ -4,59 +4,76 @@ require 'cgi'
 require 'digest/md5'
 require 'base64'
 
-class Paysera::Response
-  PAYSERA_PUBLIC_KEY = 'https://www.webtopay.com/download/public.key'
+module Paysera
+  class Response
+    PAYSERA_PUBLIC_KEY = 'https://www.webtopay.com/download/public.key'
 
-  def self.check_response(query, project_id=nil, sign_password=nil)
-    raise exception("'ss1' parameter was not found") if query[:ss1].nil?
-    raise exception("'ss2' parameter was not found") if query[:ss2].nil?
-    raise exception("'data' parameter was not found") if query[:data].nil?
-    raise exception("unable to verify 'ss2'") unless valid_ss2? query
-    raise exception("unable to verify 'ss1'") unless valid_ss1? query,
-                                                                sign_password || Paysera.sign_password
+    def initialize(query, project_id: nil, sign_password: nil)
+      raise send_error("'data' parameter was not found") if query[:data].nil?
+      raise send_error("'ss1' parameter was not found") if query[:ss1].nil?
+      raise send_error("'ss2' parameter was not found") if query[:ss2].nil?
 
-    data       = convert_to_hash decode_string(query[:data])
-    project_id ||= Paysera.project_id
+      project_id ||= Paysera.project_id
+      raise send_error("'projectid' parameter was not found") if project_id.nil?
 
-    raise exception("'projectid' mismatch") if data[:projectid].to_i != project_id
+      sign_password ||= Paysera.sign_password
+      raise send_error("'sign_password' parameter was not found") if sign_password.nil?
 
-    data
-  end
+      raise send_error("Unable to verify 'ss1'") unless valid_ss1? query[:data], query[:ss1], sign_password
+      raise send_error("Unable to verify 'ss2'") unless valid_ss2? query[:data], query[:ss2]
 
-  private
+      @data = convert_to_hash safely_decode_string(query[:data])
 
-  def self.convert_to_hash(query)
-    Hash[query.split('&').collect do |s|
-           a = s.split('=')
-           [unescape_string(a[0]).to_sym, unescape_string(a[1])]
-         end]
-  end
+      raise send_error("'projectid' mismatch") if @data[:projectid].to_i != project_id
+    end
 
-  def self.valid_ss1?(query, sign_password)
-    Digest::MD5.hexdigest(query[:data] + sign_password) == query[:ss1]
-  end
+    def sms?
+      # TODO check data with sms params
+    end
 
-  def self.valid_ss2?(query)
-    public_key = get_public_key
-    ss2        = unescape_string decode_string(query[:ss2])
-    data       = unescape_string query[:data]
+    def bank?
+      # TODO check data with bank params
+    end
 
-    public_key.verify(OpenSSL::Digest::SHA1.new, ss2, data)
-  end
+    def get_data
+      @data
+    end
 
-  def self.get_public_key
-    OpenSSL::X509::Certificate.new(open(PAYSERA_PUBLIC_KEY).read).public_key
-  end
+    private
 
-  def self.decode_string(string)
-    Base64.decode64 string.gsub('-', '+').gsub('_', '/')
-  end
+    def convert_to_hash(query)
+      Hash[query.split('&').collect do |s|
+             a = s.split('=')
+             [unescape_string(a[0]).to_sym, unescape_string(a[1])]
+           end]
+    end
 
-  def self.unescape_string(string)
-    CGI.unescape string.to_s
-  end
+    def valid_ss1?(data, ss1, sign_password)
+      Digest::MD5.hexdigest(CGI.unescape(data) + sign_password) == ss1
+    end
 
-  def self.exception(msg)
-    Exception.new msg
+    def valid_ss2?(data, ss2)
+      public_key = get_public_key
+      ss2        = unescape_string safely_decode_string(unescape_string(ss2))
+      data       = unescape_string data
+
+      public_key.verify(OpenSSL::Digest::SHA1.new, ss2, data)
+    end
+
+    def get_public_key
+      OpenSSL::X509::Certificate.new(open(PAYSERA_PUBLIC_KEY).read).public_key
+    end
+
+    def safely_decode_string(string)
+      Base64.decode64 string.gsub('-', '+').gsub('_', '/').gsub("\n", '')
+    end
+
+    def unescape_string(string)
+      CGI.unescape string.to_s
+    end
+
+    def send_error(msg)
+      Paysera::Error::Response.new msg
+    end
   end
 end
